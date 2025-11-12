0338bc57a30c1960a1214fc450ec004dfdfd4d07
Hasal Dharmagunawradana <107778591+hesxo@users.noreply.github.com>
2025-11-12 09:47:21 +0530

    Update api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:8000";
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "https://stayza-backend-production.up.railway.app/";
 
 // Define a service using a base URL and expected endpoints
 export const api = createApi({
